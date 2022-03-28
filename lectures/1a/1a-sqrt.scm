182d9b544cfd20d24cdd1d1602899126ff57547e
asherhe <github@asherhe.com>
2022-03-28 02:50:48 +0000

    Add professor implementation of sqrt

--- lectures/1a/1a-sqrt.scm
+++ lectures/1a/1a-sqrt.scm
@@ -20,4 +20,20 @@
        accuracy))
   (define (improve guess)
     (avg guess (/ x guess)))
-  (try-guess (/ x 2.0))) ; x / 2 is probably a good enough starting point
+  (try-guess (/ x 2.0))) ; x/2 is probably a good enough starting point
+
+; Professor's Implementation
+(define (square x) (* x x)) ; not included in MIT scheme but assumed to be there by the professor
+(define (average a b) (/ (+ a b) 2)) ; same with average
+
+(define (sqrt x)
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x))
+      .001))
+  (define (try guess)
+    (if (good-enough? guess)
+      guess
+      (try (improve guess))))
+  (try 1))
